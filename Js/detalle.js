e2a1c022bddc0d14f9f9a28150472216e64efc96
julinewland <jnewland@udesa.edu.ar>
2019-11-12 19:55:21 -0300

    cambios nuevos

--- Js/detalle.js
+++ Js/detalle.js
@@ -16,17 +16,52 @@ window.onload = function() {
     var lenguajeOriginal = respuesta.original_language;
     var sinopsis = respuesta.overview;
 
+    if (lenguajeOriginal == "en"){
+      lenguajeOriginal = "Ingles"
+    }
+    else {
+      lenguajeOriginal= lenguajeOriginal
+    }
+
     var total = document.querySelector("div.total");
     total.innerHTML += `
     <div class="total"><div class="imagen"><img src="http://image.tmdb.org/t/p/w200/`+ poster + `" alt=""></div><div class="detalle"><div class="display">
     <h2>`+nombre +`</h2><h4>`+estrellas+`<ion-icon name="star"></ion-icon></h4></div>
-    <h3>`+ anio +`</h3><h6>`+generos+`</h6><h6 class="">`+ lenguajeOriginal +`</h6>
+    <h3>`+ anio +`</h3><h6>Genero: `+generos+`</h6><h6 class="">Lenguaje original: `+ lenguajeOriginal +`</h6>
     <p>`+ sinopsis +`</p></div>
     </div>
     `;
 
 
   })
+  console.log("OK");
+  var idGenero = new URLSearchParams(location.search).get("id");
+  fetch("https://api.themoviedb.org/3/tv/"+ idGenero +"/recommendations?api_key=9901ee414425659325dc091c288e33c9&language=en-US&page=1")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(respuesta) {
+    console.log(respuesta);
+
+var reco = respuesta.results;
+console.log(reco);
+for (var i = 0; i <5; i++) {
+  document.querySelector(".rr1").innerHTML += "<div class='punt'><a href=detalle.html?id="+ reco[i].id +"><img src=http://image.tmdb.org/t/p/w200"+ reco[i].poster_path+"></a><h5>"+reco[i].name + "</h5><h6>"+reco[i].vote_average+"<ion-icon name='star'></ion-icon></h6></div>"
+}
+
+
+  })
+
+
+  .catch(function(error) {
+    alert("Error, perdon, vuelva mas tarde")
+  })
+
+
+
+
+
+
 }
 /*
 
--- Js/home.js
+++ Js/home.js
@@ -10,7 +10,7 @@ window.onload = function() {
 
     var serie = respuesta.results;
     for (var i = 0; i < serie.length; i++) {
-      document.querySelector(".puntuadas").innerHTML += "<div class='punt'><img src=http://image.tmdb.org/t/p/w200"+ serie[i].poster_path +"><h5>" + serie[i].name + "</h5><h6>"+serie[i].vote_average+"<ion-icon name='star'></ion-icon></h6></div>"
+      document.querySelector(".puntuadas").innerHTML += "<div class='punt'><a href=detalle.html?id="+ serie[i].id +"><img src=http://image.tmdb.org/t/p/w200"+ serie[i].poster_path +"></a><h5>" + serie[i].name + "</h5><h6>"+serie[i].vote_average+"<ion-icon name='star'></ion-icon></h6></div>"
     }
   })
   .catch(function(error) {
@@ -27,7 +27,7 @@ window.onload = function() {
 
     var favs = respuesta.results;
     for (var i = 0; i < favs.length; i++) {
-      document.querySelector(".favoritas").innerHTML += "<div class='punt'><a href=detalle.html?"+ favs[i].id +"><img src=http://image.tmdb.org/t/p/w200"+ favs[i].poster_path+"></a><h5>"+favs[i].name + "</h5><h6>"+favs[i].vote_average+"<ion-icon name='star'></ion-icon></h6></div>"
+      document.querySelector(".favoritas").innerHTML += "<div class='punt'><a href=detalle.html?id="+ favs[i].id +"><img src=http://image.tmdb.org/t/p/w200"+ favs[i].poster_path+"></a><h5>"+favs[i].name + "</h5><h6>"+favs[i].vote_average+"<ion-icon name='star'></ion-icon></h6></div>"
     }
   })
 
@@ -45,33 +45,25 @@ window.onload = function() {
 
     var aire = respuesta.results;
     for (var i = 0; i < aire.length; i++) {
-      document.querySelector(".alaire").innerHTML += "<div class='punt'><img src=http://image.tmdb.org/t/p/w200"+ aire[i].poster_path+"><h5>"+aire[i].name + "</h5><h6>"+aire[i].vote_average+"<ion-icon name='star'></ion-icon></h6></div>"
+      document.querySelector(".alaire").innerHTML += "<div class='punt'><a href=detalle.html?id="+ aire[i].id +"><img src=http://image.tmdb.org/t/p/w200"+ aire[i].poster_path+"></a><h5>"+aire[i].name + "</h5><h6>"+aire[i].vote_average+"<ion-icon name='star'></ion-icon></h6></div>"
     }
   })
   .catch(function(error) {
     alert("Error, perdon, vuelva mas tarde")
   })
-
-
-
-
-
-
 
   fetch("https://api.themoviedb.org/3/tv/latest?api_key=9901ee414425659325dc091c288e33c9&language=en-US")
   .then(function(response) {
     return response.json();
   })
   .then(function(respuesta) {
-    console.log(respuesta.results[9]);
+    console.log(respuesta);
 
-    var elegida = respuesta.results;
-    for (var i = 0; i < elegida.length; i++) {
-      document.querySelector(".favseama").innerHTML += "<img src=http://image.tmdb.org/t/p/w200/" + elegida[i].backdrop_path + "><div class='textof'><h2>Serie de la semana</h2><h3>"+ elegida[i].name+"</h3> <div class='sem'><h4>"+ elegida[i].AÑO+"</h4><h5>"+ elegida[i].vote_average+"<ion-icon name='star'></ion-icon></h5></div></div>"
-    }
-  })
+    var elegida = respuesta;
+      document.querySelector(".favsemana").innerHTML += "<img src=http://image.tmdb.org/t/p/w200"+elegida.backdrop_path+"><h2>La serie mas reciente:</h2><h3>"+ elegida.original_name+"</h3><div class='sem'><h4>"+elegida.first_air_date+"</h4><h5>"+elegida.genres+"</h5></div><p>"+ elegida.overview+"</p></div><div class='fotof'><img src=http://image.tmdb.org/t/p/w200"+elegida.poster_path+ "></div>"
+    })
   .catch(function(error) {
-    alert("Error, perdon, vuelva mas tarde")
+
 })
 
 }
