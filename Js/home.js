c471cb0ed934268ce3002c793c638ae6b14f6139
julinewland <jnewland@udesa.edu.ar>
2020-05-13 14:04:02 -0300

    actualizacion

--- Js/home.js
+++ Js/home.js
@@ -62,10 +62,10 @@ fetch("https://api.themoviedb.org/3/tv/popular?api_key=9901ee414425659325dc091c2
 .then(function(response) {
   return response.json();
 })
-.then(function(respuesta) {
-  console.log(respuesta.results[9]);
+.then(function(data) {
+  console.log(data);
 
-  var favs = respuesta.results;
+  var favs = data.results;
   for (var i = 0; i < 1; i++) {
     document.querySelector(".favsemana").innerHTML += "<div class='total'><div class='imagen'><a href=detalle.html?id="+ favs[i].id +"><img src=http://image.tmdb.org/t/p/w200"+ favs[i].poster_path +"></a></div><div class='detalle'><div class='display'><h2 class='copia'>"+ favs[i].original_name +"</h2><h4 class='copia'>"+favs[i].vote_average+"<ion-icon name='star'></ion-icon></h4></div><h3 class='copia'>"+ favs[i].first_air_date +"</h3><p class='copia'>"+ favs[i].overview+ "</p></div></div>"
 
